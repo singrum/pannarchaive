299a82edbf254a0257508e8e31d9554a5e1582d0
Singrum <103807515+singrum@users.noreply.github.com>
2022-12-08 12:27:04 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -4,17 +4,19 @@ function main(){
     const index_box = document.querySelector("#index-box");
     const img_space = document.querySelector("#img-space");
 
-    let index = 65222239;
+    let index = 65230050;
+    let img_per_page = 30;
+
     index_box.value = index;
     search();
 
     arrow_back.addEventListener("click", ()=>{
-        index_box.value = parseInt(index_box.value) + 20;
+        index_box.value = parseInt(index_box.value) + img_per_page;
         search();
     })
 
     arrow_forward.addEventListener("click", ()=>{
-        index_box.value = parseInt(index_box.value) - 20;
+        index_box.value = parseInt(index_box.value) - img_per_page;
         search();
     })
     index_box.addEventListener('keypress', function (e) {
@@ -26,7 +28,7 @@ function main(){
     function search(){
         let html = "";
         index = index_box.value;
-        for(let i = 0; i < 20; i++){
+        for(let i = 0; i < img_per_page; i++){
             html += `<img class="image" src="http://fimg5.pann.com/new/download.jsp?FileID=${index - i}">`
         }
         img_space.innerHTML = html
