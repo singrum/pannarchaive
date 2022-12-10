a9e8f787f18626e1cf6b7bde76f08ec3acc83b4b
Kang Hyomin <103807515+singrum@users.noreply.github.com>
2022-12-10 14:16:10 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,216 +1,56 @@
-let camera, scene, renderer, controls;
-window.addEventListener("resize", onResize, false);
-function onResize(){
-    camera.aspect = window.innerWidth / (window.innerHeight);
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
-function init(){
-
-    /////////////////swife detector////////////////////////////////////////
-    let curr_direction = 0
-    document.addEventListener('touchstart', handleTouchStart, false);        
-    document.addEventListener('touchmove', handleTouchMove, false);
-
-    let xDown = null;                                                        
-    let yDown = null;
-
-    function getTouches(evt) {
-    return evt.touches ||             
-            evt.originalEvent.touches; 
-    }                                                     
-                                                                            
-    function handleTouchStart(evt) {
-        const firstTouch = getTouches(evt)[0];                                      
-        xDown = firstTouch.clientX;                                      
-        yDown = firstTouch.clientY;                                      
-    };                                                
-                                                                            
-    function handleTouchMove(evt) {
-        if ( ! xDown || ! yDown ) {
-            return;
-        }
-
-        let xUp = evt.touches[0].clientX;                                    
-        let yUp = evt.touches[0].clientY;
-
-        let xDiff = xDown - xUp;
-        let yDiff = yDown - yUp;
-                                                                            
-        if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-            if ( xDiff > 0 ) {
-                curr_direction = 3;
-            } else {
-                curr_direction = 2;
-            }                       
-        } else {
-            if ( yDiff > 0 ) {
-                curr_direction = 0;
-            } else { 
-                curr_direction = 1;
-            }                                                                 
-        }
-        /* reset values */
-        xDown = null;
-        yDown = null;                                             
-    };
-    ////////////////////////////////////////////////////////////////
-
-
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    camera.position.set(-200,200,50);
-    camera.lookAt(scene.position);
-
-
-    let spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.position.set(-40, 60, 70);
-    // spotLight.castShadow = true;
-    scene.add(spotLight);
+function main(){
+    const arrow_back = document.querySelector("#arrow-back");
+    const arrow_forward = document.querySelector("#arrow-forward");
+    const index_box = document.querySelector("#index-box");
+    const img_space = document.querySelector("#img-space");
+    const modal_element = document.querySelector("#zoom-modal");
+    const modal = new bootstrap.Modal(modal_element, {});
     
 
+    let index = 65230050;
+    let img_per_page = 30;
 
-    let plane = new THREE.Mesh(
-        new THREE.PlaneGeometry(100,100,1,1),
-        new THREE.MeshLambertMaterial({color: 0xffffff})
-        );
-    plane.rotation.x = -0.5 * Math.PI;
-    plane.position.set(0,0,0);
-    // plane.receiveShadow = true;
-    scene.add(plane);
+    index_box.value = index;
+    search();
 
-    const radius = 2;
-    let apple = new THREE.Mesh(
-        new THREE.SphereGeometry( radius, 32, 16 ),
-        new THREE.MeshLambertMaterial({color: 0xffff00})
-    );
-    appleCoordinate = {x:0, z: 0};
-    scene.add(apple);
-    
-    const xLength = 5;
-    const yLength = 10;
-    const zLength = 15;
-    let cube = new THREE.Mesh(
-        new THREE.BoxGeometry(xLength,yLength,zLength),
-        new THREE.MeshLambertMaterial({color: 0x00ffff})
-    );
-    cube.matrixAutoUpdate = false;
-    // cube.castShadow = true;
-    scene.add(cube);
+    arrow_back.addEventListener("click", ()=>{
+        index_box.value = parseInt(index_box.value) + img_per_page;
+        search();
+    })
 
-    let i = 0;
-    const increment = 10;
-    let start = true
-    let worldX = xLength, worldY = yLength, worldZ = zLength;
-    let length = {x : xLength, y : yLength, z : zLength};
-    let coordinate = {x : 0, z : 0};
-    let indicator = {x : 0, y : 1, z : 2};
-    let direction = 0;
-    let staticMatrix = new THREE.Matrix4();
-    function renderScene(){
+    arrow_forward.addEventListener("click", ()=>{
+        index_box.value = parseInt(index_box.value) - img_per_page;
+        search();
+    })
+    index_box.addEventListener('keypress', function (e) {
+        if (e.key === 'Enter') {
+            search();
+        }
+    });
+
+    function search(){
+        let html = "";
+        index = index_box.value;
+        for(let i = 0; i < img_per_page; i++){
+            html += `<img class="image" src="https://thumb.pann.com/tc_150x150/http://fimg5.pann.com/new/download.jsp?FileID=${index - i}">`;
+
+        }
+        img_space.innerHTML = html;
+
+        document.querySelectorAll(".image").forEach(
+            a=>a.addEventListener("click", ()=>{
+                let curr_index = new URL(a.getAttribute("src")).searchParams.get("FileID")
+                
+                modal_element.querySelector(".modal-title").innerHTML = curr_index;
+                
+                modal_element.querySelector(".modal-body").innerHTML = `<img class="full-image" src="https://fimg5.pann.com/new/download.jsp?FileID=${curr_index}"></img>`
+                modal.show()}
+                
+            )
+        )
         
-        camera.lookAt(scene.position);
-        if(start){
-            i += 0.05;
-            //x+
-            if(direction === 0){
-                cube.matrix = new THREE.Matrix4().makeTranslation(worldX/2 + coordinate.x,0,coordinate.z).multiply(
-                    new THREE.Matrix4().makeRotationZ(-i).multiply(
-                    new THREE.Matrix4().makeTranslation(-worldX/2,worldY/2,0).multiply(
-                        staticMatrix
-                    )))
-                if (i >= Math.PI/2){
-                    start = false
-                    coordinate.x += worldX/2 + worldY/2;
-                    staticMatrix = new THREE.Matrix4().makeRotationZ(Math.PI / 2).multiply(staticMatrix);
-                    [worldX, worldY] = [worldY, worldX];
-                    [indicator.x, indicator.y] = [indicator.y, indicator.x]
-                }
-            }            
-            //x-
-            else if(direction === 1){
-                cube.matrix = new THREE.Matrix4().makeTranslation(-worldX/2 + coordinate.x,0,coordinate.z).multiply(
-                    new THREE.Matrix4().makeRotationZ(i).multiply(
-                    new THREE.Matrix4().makeTranslation(worldX/2,worldY/2,0).multiply(
-                        staticMatrix
-                    )))
-                if (i >= Math.PI/2){
-                    start = false
-                    coordinate.x -= worldX/2 + worldY/2;
-                    staticMatrix = new THREE.Matrix4().makeRotationZ(Math.PI / 2).multiply(staticMatrix);
-                    [worldX, worldY] = [worldY, worldX];
-                    [indicator.x, indicator.y] = [indicator.y, indicator.x]
-                }                
-            }            
-            //z+
-            else if(direction === 2){
-                cube.matrix = new THREE.Matrix4().makeTranslation(coordinate.x,0,worldZ/2 + coordinate.z).multiply(
-                    new THREE.Matrix4().makeRotationX(i).multiply(
-                    new THREE.Matrix4().makeTranslation(0,worldY/2, -worldZ/2).multiply(
-                        staticMatrix
-                    )))
-                if (i >= Math.PI/2){
-                    start = false
-                    coordinate.z += worldZ/2 + worldY/2;
-                    staticMatrix = new THREE.Matrix4().makeRotationX(Math.PI / 2).multiply(staticMatrix);
-                    [worldZ, worldY] = [worldY, worldZ];
-                    [indicator.z, indicator.y] = [indicator.y, indicator.z]
-                }
-            }
-            //z-
-            else if(direction === 3){
-                cube.matrix = new THREE.Matrix4().makeTranslation(coordinate.x,0,-worldZ/2 + coordinate.z).multiply(
-                    new THREE.Matrix4().makeRotationX(-i).multiply(
-                    new THREE.Matrix4().makeTranslation(0,worldY/2, worldZ/2).multiply(
-                        staticMatrix
-                    )))
-                if (i >= Math.PI/2){
-                    start = false
-                    coordinate.z -= worldZ/2 + worldY/2;
-                    staticMatrix = new THREE.Matrix4().makeRotationX(Math.PI / 2).multiply(staticMatrix);
-                    [worldZ, worldY] = [worldY, worldZ];
-                    [indicator.z, indicator.y] = [indicator.y, indicator.z]
-                }
-            }
-
-
-            
-        }
-        else{
-            if ((Math.abs(appleCoordinate.x - coordinate.x) <= worldX/2 + radius) && 
-            (Math.abs(appleCoordinate.z - coordinate.z) <= worldZ/2 + radius)){
-                if(indicator.y === 0){
-                    staticMatrix = new THREE.Matrix4().makeScale(x = (length.x + increment) / length.x, y = 1, z = 1).multiply(staticMatrix);
-                    length.x += increment;
-                }
-                else if(indicator.y === 1){
-                    staticMatrix = new THREE.Matrix4().makeScale(x = 1, y = (length.y + increment) / length.y, z = 1).multiply(staticMatrix);
-                    length.y += increment;
-                }
-                else if(indicator.y === 2){
-                    staticMatrix = new THREE.Matrix4().makeScale(x = 1, y = 1, z = (length.z + increment) / length.z).multiply(staticMatrix);
-                    length.z += increment;
-                }
-                console.log(indicator)
-                worldY += increment;
-            }
-            direction = curr_direction;
-            i = 0;
-            start = true;
-        }
-        // controls.update();
-        requestAnimationFrame(renderScene);
-        renderer.render(scene, camera);
     }
-
     
-    //renderer
-    renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(0xEEEEEE);
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    // renderer.shadowMap.enabled = true;
-    document.body.appendChild(renderer.domElement);
-    // controls = new THREE.OrbitControls( camera, renderer.domElement );
-    renderScene();
 }
-window.onload = init;
+
+window.onload = main
